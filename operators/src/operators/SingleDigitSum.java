63c47841450c725e908dbe919ee15d8bd73cac00
alokito2 <betterluct428@gmail.com>
2025-09-04 11:58:22 +0530

    single digit

--- operators/src/operators/SingleDigitSum.java
+++ operators/src/operators/SingleDigitSum.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class SingleDigitSum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		try {
 		System.out.print("Enter the number : ");
 		int num = sc.nextInt();
 		int result = num;
@@ -17,6 +18,10 @@ public class SingleDigitSum {
 			result = sum;
 		}
 		System.out.println("The single-digit sum is : "+ result);
+		}catch(Exception e) {
+			System.out.println("please enter valid number");
+		}
+		
 		sc.close();
 		}
 	
